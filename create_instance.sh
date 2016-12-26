e6009ef3f0f8b9a355b3a940a4f86c5127a8e4bf
boliu <bo@whisper.sh>
2016-12-26 15:50:22 -0800

    *

--- create_instance.sh
+++
@@ -1,5 +0,0 @@
-gcloud compute instances create shopping_instance \
-    --image=ubuntu-1404 \
-    --machine-type=g1-micro \
-    --metadata-from-file startup-script=./startup_script.sh \
-    --zone us-central1-a
