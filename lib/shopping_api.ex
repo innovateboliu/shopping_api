f832aab46476852b155d049ec4b924713974dfd6
boliu <bo@whisper.sh>
2016-12-26 15:05:06 -0800

    *

--- lib/shopping_api.ex
+++ lib/shopping_api.ex
@@ -9,7 +9,7 @@ defmodule ShoppingApi do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(ShoppingApi.Repo, []),
+      #supervisor(ShoppingApi.Repo, []),
       # Start the endpoint when the application starts
       supervisor(ShoppingApi.Endpoint, []),
       # Start your own worker by calling: ShoppingApi.Worker.start_link(arg1, arg2, arg3)
