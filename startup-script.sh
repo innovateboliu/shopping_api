6dac9eb5a01ded85fa00e86ad646b740482c15c8
boliu <bo@whisper.sh>
2016-12-27 11:10:19 -0800

    *

--- startup-script.sh
+++ startup-script.sh
@@ -11,6 +11,11 @@ yes | mix archive.install https://github.com/phoenixframework/archives/raw/maste
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
 
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
+mv cloud_sql_proxy.linux.amd64 cloud_sql_proxy
+mod +x cloud_sql_proxy
+./cloud_sql_proxy -instances=beauty-148304:us-central1:mysql=tcp:3306 &
+
 git config --global credential.helper gcloud.sh
 git clone https://source.developers.google.com/p/$PROJECTID /opt/app
 
